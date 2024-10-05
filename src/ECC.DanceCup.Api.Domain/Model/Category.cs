4983e3dc1c445c0b97be74b580cab50a7e04a7cb
Yaroslav Senchin <yrik200315@gmail.com>
2024-10-05 13:28:42 +0300

    добавлен класс Category

--- src/ECC.DanceCup.Api.Domain/Model/Category.cs
+++ src/ECC.DanceCup.Api.Domain/Model/Category.cs
@@ -1,4 +1,6 @@
 ﻿using ECC.DanceCup.Api.Domain.Core;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ECC.DanceCup.Api.Domain.Model;
 
@@ -7,7 +9,20 @@ namespace ECC.DanceCup.Api.Domain.Model;
 /// </summary>
 public class Category : Entity<CategoryId>
 {
-    public Category(CategoryId id, DateTime createdAt, DateTime changedAt) : base(id, createdAt, changedAt)
+    private readonly List<DanceId> _dances;
+
+    public Category(
+        CategoryId id,
+        DateTime createdAt,
+        DateTime changedAt,
+        List<DanceId> dances)
+        : base(id, createdAt, changedAt)
     {
+        _dances = dances;
     }
+
+    /// <summary>
+    /// Список танцев категории
+    /// </summary>
+    public IReadOnlyCollection<DanceId> Dances => _dances;
 }

8b151927cc4238c802f22cc0eabd718452da0853
Yaroslav Senchin <yrik200315@gmail.com>
2024-10-05 22:54:40 +0300

    переписан UseCase

--- src/ECC.DanceCup.Api.Application/UseCases/CreateTournament/Contract.cs
+++ src/ECC.DanceCup.Api.Application/UseCases/CreateTournament/Contract.cs
@@ -6,7 +6,7 @@ namespace ECC.DanceCup.Api.Application.UseCases.CreateTournament;
 
 public static partial class CreateTournamentUseCase
 {
-    public record Command() : IRequest<Result<CommandResponse>>;
+    public record Command(UserId userId, TournamentName name, List<Category> catigories) : IRequest<Result<CommandResponse>>;
 
     public record CommandResponse(TournamentId TournamentId);
 }

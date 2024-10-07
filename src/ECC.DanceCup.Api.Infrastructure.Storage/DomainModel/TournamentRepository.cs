61b32f6b72f51da654aa566e8e2965459c4e371f
ZISRF <zeleninivans@gmail.com>
2024-10-07 16:46:09 +0300

    9: Довалено in-memory хранилище

--- src/ECC.DanceCup.Api.Infrastructure.Storage/DomainModel/TournamentRepository.cs
+++ src/ECC.DanceCup.Api.Infrastructure.Storage/DomainModel/TournamentRepository.cs
@@ -1,22 +1,36 @@
 ﻿using ECC.DanceCup.Api.Application.Abstractions.Storage.DomainModel;
 using ECC.DanceCup.Api.Domain.Model;
+using ECC.DanceCup.Api.Utils.Extensions;
 
 namespace ECC.DanceCup.Api.Infrastructure.Storage.DomainModel;
 
 public class TournamentRepository : ITournamentRepository
 {
+    private readonly Dictionary<TournamentId, Tournament> _tournaments = new();
+    
     public Task<Tournament?> FindAsync(TournamentId tournamentId, CancellationToken cancellationToken)
     {
-        return Task.FromResult((Tournament?)null);
+        if (_tournaments.TryGetValue(tournamentId, out var tournament))
+        {
+            tournament = null;
+        }
+        
+        return Task.FromResult(tournament);
     }
 
     public Task<TournamentId> AddAsync(Tournament tournament, CancellationToken cancellationToken)
     {
-        return Task.FromResult(TournamentId.Empty);
+        var tournamentId = TournamentId.From(_tournaments.Count + 1).AsRequired();
+        
+        _tournaments.Add(tournamentId, tournament);
+        
+        return Task.FromResult(tournamentId);
     }
 
     public Task UpdateAsync(Tournament tournament, CancellationToken cancellationToken)
     {
+        _tournaments[tournament.Id] = tournament;
+        
         return Task.CompletedTask;
     }
 }

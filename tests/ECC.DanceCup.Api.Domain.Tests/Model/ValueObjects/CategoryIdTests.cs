7c5ea21e038f9b53714a92b49d18112f672cf25a
Yaroslav Senchin <yrik200315@gmail.com>
2024-10-10 17:30:54 +0300

    добавлены тесты для CategoryId

---
+++ tests/ECC.DanceCup.Api.Domain.Tests/Model/ValueObjects/CategoryIdTests.cs
@@ -0,0 +1,43 @@
+﻿using ECC.DanceCup.Api.Domain.Model;
+using FluentAssertions;
+
+namespace ECC.DanceCup.Api.Domain.Tests.Model.ValueObjects;
+
+public class CategoryIdTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(int.MaxValue)]
+    [InlineData(long.MaxValue)]
+    public void Create_FromIncorrectValue_ShouldNotBeNull(long value)
+    {
+        //Arrange
+
+        //Act
+
+        var categoryId = CategoryId.From(value); 
+
+        //Assert
+
+        categoryId.Should().NotBeNull();
+        categoryId.Value.Value.Should().Be(value);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(int.MinValue)]
+    [InlineData(long.MinValue)]
+    public void Create_FromIncorrectValue_ShouldBeNull(long value)
+    {
+        //Arrange
+
+        //Act
+
+        var categoryId= CategoryId.From(value);
+
+        //Assert
+
+        categoryId.Should().BeNull();
+    }
+}

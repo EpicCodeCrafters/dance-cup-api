f86ad9e0149f6ac307ed28df4374e751c50c3a49
Yaroslav Senchin <yrik200315@gmail.com>
2024-10-19 18:21:38 +0300

    added UseCases

---
+++ src/ECC.DanceCup.Api.Application/UseCases/ReopenTournamentRegistration/Contract.cs
@@ -0,0 +1,10 @@
+﻿using ECC.DanceCup.Api.Domain.Model.TournamentAggregate;
+using FluentResults;
+using MediatR;
+
+namespace ECC.DanceCup.Api.Application.UseCases.ReopenTournamentRegistration;
+
+public static partial class ReopenTournamentRegistrationUseCase
+{
+    public record Command(TournamentId TournamentId) : IRequest<Result>;
+}
---
+++ src/ECC.DanceCup.Api.Application/UseCases/ReopenTournamentRegistration/Handler.cs
@@ -0,0 +1,38 @@
+﻿using ECC.DanceCup.Api.Application.Abstractions.Storage.DomainModel;
+using ECC.DanceCup.Api.Application.Errors;
+using FluentResults;
+using MediatR;
+
+namespace ECC.DanceCup.Api.Application.UseCases.ReopenTournamentRegistration;
+
+public static partial class ReopenTournamentRegistrationUseCase
+{
+    public class CommandHandler
+    {
+        private readonly ITournamentRepository _tournamentRepository;
+
+        public CommandHandler(ITournamentRepository tournamentRepository)
+        {
+            _tournamentRepository = tournamentRepository;
+        }
+
+        public async Task<Result> Handle(Command command,CancellationToken cancellationToken)
+        {
+            var tournament = await _tournamentRepository.FindAsync(command.TournamentId, cancellationToken);
+            if (tournament == null)
+            {
+                return new TournamentNotFoundError(command.TournamentId);
+            }
+
+            var reopenRegistrationResult = tournament.ReopenRegistration();
+            if (reopenRegistrationResult.IsFailed)
+            {
+                return reopenRegistrationResult;
+            }
+
+            await _tournamentRepository.UpdateAsync(tournament, cancellationToken);
+
+            return Result.Ok();
+        }
+    }
+}

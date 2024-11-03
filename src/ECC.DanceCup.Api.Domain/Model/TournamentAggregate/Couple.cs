b46bff1431b07f63650be3813b8d25daeeac7110
ZISRF <zeleninivans@gmail.com>
2024-11-03 18:05:22 +0300

    Фикс

--- src/ECC.DanceCup.Api.Domain/Model/TournamentAggregate/Couple.cs
+++ src/ECC.DanceCup.Api.Domain/Model/TournamentAggregate/Couple.cs
@@ -2,6 +2,9 @@
 
 namespace ECC.DanceCup.Api.Domain.Model.TournamentAggregate;
 
+/// <summary>
+/// Танцевальная пара
+/// </summary>
 public class Couple :  Entity<CoupleId>
 {
     public Couple(
@@ -22,15 +25,33 @@ public class Couple :  Entity<CoupleId>
         SecondTrainerFullName = secondTrainerFullName;
     }
     
+    /// <summary>
+    /// Идентификатор турнира, в котором участвует пара
+    /// </summary>
     public TournamentId TournamentId { get; }
     
+    /// <summary>
+    /// Полное имя первого участника пары
+    /// </summary>
     public CoupleParticipantFullName FirstParticipantFullName { get; }
     
+    /// <summary>
+    /// Второе имя участника пары
+    /// </summary>
     public CoupleParticipantFullName? SecondParticipantFullName { get; }
     
+    /// <summary>
+    /// Название организации, в которой пара занимается танцами
+    /// </summary>
     public CoupleDanceOrganizationName? DanceOrganizationName { get; }
     
+    /// <summary>
+    /// Полное имя первого тренера пары
+    /// </summary>
     public CoupleTrainerFullName? FirstTrainerFullName { get; }
     
+    /// <summary>
+    /// Полное имя второго тренера пары
+    /// </summary>
     public CoupleTrainerFullName? SecondTrainerFullName { get; }
 }

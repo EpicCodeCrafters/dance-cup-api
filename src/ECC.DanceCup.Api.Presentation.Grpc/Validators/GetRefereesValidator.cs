cd7e2206d46a5b9a6d2f5aa8abcf0ed84ce90f9c
Yaroslav Senchin <93538385+KapikPro@users.noreply.github.com>
2025-07-21 23:49:27 +0300

    hot fix

--- src/ECC.DanceCup.Api.Presentation.Grpc/Validators/GetRefereesValidator.cs
+++ src/ECC.DanceCup.Api.Presentation.Grpc/Validators/GetRefereesValidator.cs
@@ -7,8 +7,6 @@ public class GetRefereesValidator : AbstractValidator<GetRefereesRequest>
 {
     public GetRefereesValidator()
     {
-        RuleFor(request => request.FullName).IsValidRefereeFullName().Unless(value => value is null);
-
         RuleFor(request => request.PageNumber).IsValidPageNumber();
 
         RuleFor(request => request.PageSize).IsValidPageSize();

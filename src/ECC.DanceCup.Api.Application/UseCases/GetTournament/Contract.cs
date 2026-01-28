df67b748fc77c35a55a51135b0076dab6da2b7ba
Yaroslav Senchin <93538385+KapikPro@users.noreply.github.com>
2026-01-28 23:38:54 +0300

    added GetTournament with hidden error

---
+++ src/ECC.DanceCup.Api.Application/UseCases/GetTournament/Contract.cs
@@ -0,0 +1,13 @@
+﻿using ECC.DanceCup.Api.Application.Abstractions.Models.Views;
+using ECC.DanceCup.Api.Domain.Model.TournamentAggregate;
+using FluentResults;
+using MediatR;
+
+namespace ECC.DanceCup.Api.Application.UseCases.GetTournament;
+
+public static partial class GetTournamentUseCase
+{
+    public record Query(TournamentId TournamentId) : IRequest<Result<QueryResponse>>;
+    
+    public record QueryResponse(TournamentView Tournament);
+}
---
+++ src/ECC.DanceCup.Api.Application/UseCases/GetTournament/Handler.cs
@@ -0,0 +1,26 @@
+﻿using ECC.DanceCup.Api.Application.Abstractions.Storage.DomainModel;
+using ECC.DanceCup.Api.Application.Abstractions.Storage.ReadModel;
+using FluentResults;
+using MediatR;
+
+namespace ECC.DanceCup.Api.Application.UseCases.GetTournament;
+
+public static partial class GetTournamentUseCase
+{
+    public class QueryHandler : IRequestHandler<Query, Result<QueryResponse>>
+    {
+        private readonly ITournamentViewRepository _tournamentRepository;
+
+        public QueryHandler(ITournamentViewRepository tournamentRepository)
+        {
+            _tournamentRepository = tournamentRepository;
+        }
+
+        public async Task<Result<QueryResponse>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var tournament = await _tournamentRepository.FindOneAsync(request.TournamentId, cancellationToken);
+
+            return new QueryResponse(tournament);
+        }
+    }
+}
